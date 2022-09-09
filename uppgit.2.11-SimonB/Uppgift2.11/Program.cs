b73990055dfa820ea6136b85c7d027c531958995
E-Svensson <111962443+E-Svensson@users.noreply.github.com>
2022-09-09 10:48:08 +0200

    uppgift2.11 klar

---
+++ uppgit.2.11-SimonB/Uppgift2.11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Uppgift_2._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin mätt i meter?");
+            string längdText = Console.ReadLine();
+            double längd = double.Parse(längdText);
+            Console.WriteLine("Hur långt hoppade Alma i meter?");
+            string längd2text = Console.ReadLine();
+            double längd2 = double.Parse(längd2text);
+            double skillnad = längd - längd2;
+            Console.WriteLine("Elin hoppade " + skillnad + " meter längre.");
+        }
+    }
+}
