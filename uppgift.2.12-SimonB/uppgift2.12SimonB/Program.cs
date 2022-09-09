95eb29042c35173a440380eed00921627e327c1c
E-Svensson <111962443+E-Svensson@users.noreply.github.com>
2022-09-09 11:09:57 +0200

    uppgift2.12 klar Simon B

---
+++ uppgift.2.12-SimonB/uppgift2.12SimonB/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Uppgift_2._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många dagar vill du hyra bilen?");
+            string strDagar = Console.ReadLine();
+            Console.WriteLine("Hur många kilometer ska du ströga med bilen i kilometer?"); 
+            string strStrög = Console.ReadLine();
+
+            int intDagar = int.Parse(strDagar);
+            int intStrög = int.Parse(strStrög);
+
+            int Summa = 300 + intDagar * 500 + intStrög * 1 + 500;
+
+            Console.WriteLine("Strögen kommer att kosta dig " + Summa + "kr");
+
+            
+        }
+    }
+}
